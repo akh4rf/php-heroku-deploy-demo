fe430e2806207d81adf80ca268c45327aff7e546
Austin Houck <akh4rf@virginia.edu>
2021-10-28 02:29:34 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/jpg" href="./heroku-logos/stroke-gradient-transparent.png" />
+  <title>PHP Heroku Deployment Demo</title>
+</head>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap');
+
+  body {
+    background: #430098;
+    margin: 0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    height: 300px;
+    width: 300px;
+  }
+
+  h1 {
+    font-family: 'Noto Sans', sans-serif;
+    color: white;
+  }
+</style>
+
+<body>
+
+  <img src="./heroku-logos/stroke-white-transparent.png" alt="Heroku Logo">
+  <h1>Successfully deployed PHP app to Heroku!</h1>
+
+</body>
+
+</html>
